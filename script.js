3fef8019f70fb9a9a20af7bbfb2bd2d59177b962
unknown <doxplay10@gmail.com>
2021-11-24 15:03:38 -0300

    verifica vez do jogador e adiciona figuras

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+var vez = 1;
+var vencedor = "";
+
+$(document).ready(function(){
 $(".casa").click(function(){
   var fundo = $(this).css("background-image");
 if(fundo == "none" || fundo == ""){
-//
+var fig = "url("+vez.toString()+".jpg)";
+$(this).css("background",fig);
+vez = (vez == 1 ? 2:1);
+verificarFimDeJogo();
 }
 });
+});
+
+function verificarFimDeJogo(){
+
+}
